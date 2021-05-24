07ee54f3fda5790ad595e6489051d6288e70f4ca
Charlie Porth <charlieporth1@gmail.com>
2021-05-24 13:54:13 -0500

    Updated perect

--- predict.py
+++ predict.py
@@ -10,19 +10,27 @@ parser.add_argument('image_path',help="image path")
 parser.add_argument('model_path',help="model path")
 parser.add_argument('--top_k',type= int ,help="top required probabilities", required = False, default = 3)
 parser.add_argument('--category_names',
-                    help="file path for a json file to map the category names",
+                    help="file path for a json file to map the plant category names",
                     required = False,
                     default = None)
 args = parser.parse_args()
+def get_class_names(json_file):
+    with open(json_file, 'r') as f:
+        class_names = json.load(f)
+    # Remapping as Class names have index starting from 1 to 102, whereas the datasets have label indices from 0 to 101
+    class_new_names = dict()
+    for key in class_names:
+        class_new_names[str(int(key)-1)] = class_names[key]
+    return class_new_names
 
-def process_img(img) :
+def process_img(img):
     img = tf.convert_to_tensor(img)
     img = tf.image.resize(img , (224,224))
     img /= 225
     img = img.numpy()
     return img
 
-def predict(img_path, model, top_k) :
+def predict(img_path, model, top_k):
         img = Image.open(img_path)
         img_array = np.asarray(img)
         processed_img = process_img(img_array)
@@ -31,22 +39,15 @@ def predict(img_path, model, top_k) :
         probabilities , classes = tf.math.top_k(prediction, top_k)
         return list(probabilities.numpy()[0]) , list(classes.numpy()[0])
 
-def main (image_path , model_path , top_k, category_names) :
-    model = tf.keras.models.load_model(model_path,custom_objects={'KerasLayer':hub.KerasLayer})
+def main(image_path , model_path , top_k, category_names):
+    model = tf.keras.models.load_model(model_path, custom_objects={'KerasLayer':hub.KerasLayer})
     prob , classes = predict(image_path , model , top_k)
 
     print(f'probabilites    = {prob}')
     print(f'classes numbers = {classes}')
 
-    if category_names != None :
-        with open('label_map.json', 'r') as f:
-            class_names = json.load(f)
-        classes_names_list = []
-        for i in classes :
-            classes_names_list.append(class_names[str(i +1)])
-        print (f'classes names   = {classes_names_list}')
-    else :
-        classes_names_list = None
+    
+  
 
     return prob , classes , classes_names_list
 
@@ -55,4 +56,9 @@ def main (image_path , model_path , top_k, category_names) :
 
 
 if __name__ == '__main__' :
-    main(args.image_path , args.model_path , args.top_k , args.category_names)
+    if args.category_names != None:
+       classes_names_list = get_class_names(args.category_names)
+    else:
+       classes_names_list = None
+        
+    main(args.image_path , args.model_path , args.top_k , classes_names_list)
